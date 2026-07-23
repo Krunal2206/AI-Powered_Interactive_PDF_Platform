153bf9897c60491298f848fd901cf377e616ebd0
Krunal Thakar <kru.dipnil@gmail.com>
2026-07-23 12:41:59 -0400

    feat: add nonce retrieval for enhanced security in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { headers } from "next/headers";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
@@ -29,11 +30,13 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const nonce = (await headers()).get("x-nonce") ?? "";
+
   return (
     <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en">
