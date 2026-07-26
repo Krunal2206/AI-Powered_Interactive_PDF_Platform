8eb46ff04b7209ab3e659d53e046e2970af564e8
Krunal Thakar <kru.dipnil@gmail.com>
2026-07-26 15:42:57 -0400

    feat: add Footer component to HomeLayout for improved site navigation

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,14 +1,16 @@
 import HomeNavbar from "@/components/HomePage/HomeNavbar";
+import Footer from "@/components/Footer";
 
 export default function HomeLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <>
       <HomeNavbar />
       <main>{children}</main>
+      <Footer />
     </>
   );
 }
