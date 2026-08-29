d7bb8865741f4779e4464e64ba20850797290763
Krunal Thakar <kru.dipnil@gmail.com>
2026-08-28 20:01:54 -0400

    feat: add error handling component to dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,11 @@
 import Navbar from '@/components/DashboardPage/Navbar';
-import { ErrorBoundary } from '@/components/ErrorBoundary';
 import { ClerkLoaded } from '@clerk/nextjs'
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkLoaded>
-      <ErrorBoundary section="Dashboard">
-        <Navbar />
-        {children}
-      </ErrorBoundary>
+      <Navbar />
+      {children}
     </ClerkLoaded>
   );
 }
