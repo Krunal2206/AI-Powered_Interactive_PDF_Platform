911e787ce118ec21fc8ffab909b6a3853fb79ddc
Krunal Thakar <kru.dipnil@gmail.com>
2026-09-16 01:50:32 -0400

    feat: rename HomeLayout to PricingLayout and add Footer component

--- app/pricing/layout.tsx
+++ app/pricing/layout.tsx
@@ -1,14 +1,16 @@
 import Header from "@/components/PricingPage/Header";
+import Footer from "@/components/Footer";
 
-export default function HomeLayout({
+export default function PricingLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <>
       <Header />
       <main>{children}</main>
+      <Footer />
     </>
   );
 }
